ef156264f7330f25e5479c2e0abf95f15c9d4f93
Yberion <Brandon.Largeau@gmail.com>
2024-07-16 18:42:32 +0200

    build: update to Angular 18

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { HttpClientModule } from '@angular/common/http';
+import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
 import { LOCALE_ID, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
@@ -9,17 +9,17 @@ import { SharedModule } from 'app/shared/shared.module';
 
 @NgModule({
   declarations: [AppComponent],
+  bootstrap: [AppComponent],
   imports: [
     BrowserModule,
-    HttpClientModule,
     AppRoutingModule,
     BrowserAnimationsModule,
     SharedModule,
     BaseModule
   ],
   providers: [
-    { provide: LOCALE_ID, useValue: 'en' }
-  ],
-  bootstrap: [AppComponent]
+    { provide: LOCALE_ID, useValue: 'en' },
+    provideHttpClient(withInterceptorsFromDi())
+  ]
 })
 export class AppModule {}
