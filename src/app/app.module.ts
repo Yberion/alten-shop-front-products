6bf1fd8dcdb5d9152aa3eedf70eb9d5a569db9c4
Yberion <Brandon.Largeau@gmail.com>
2024-07-17 12:03:47 +0200

    refactor: rename app routing module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,8 +5,8 @@ import {
 import { LOCALE_ID, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app.routing.module';
 
 import { CoreModule } from './core/core.module';
 import { SharedModule } from './shared/shared.module';
