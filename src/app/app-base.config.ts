3dde18c9ee7003aef2d132959e2bf0f0c93f9293
Yberion <Brandon.Largeau@gmail.com>
2024-07-17 13:03:46 +0200

    refactor: setup resolveNavigationPromiseOnError

--- src/app/app-base.config.ts
+++ src/app/app-base.config.ts
@@ -1,13 +1,13 @@
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { LOCALE_ID, provideZoneChangeDetection, type ApplicationConfig } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import { provideRouter, withRouterConfig } from '@angular/router';
 import { MessageService } from 'primeng/api';
 import { appRoutes } from './app.routes';
 import { errorInterceptor } from './core/interceptors/error/error.interceptor';
 
 export const appBaseConfig: ApplicationConfig = {
   providers: [
-    provideRouter(appRoutes),
+    provideRouter(appRoutes, withRouterConfig({ resolveNavigationPromiseOnError: true }),),
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideHttpClient(withInterceptors([errorInterceptor])),
     { provide: LOCALE_ID, useValue: 'en' },
