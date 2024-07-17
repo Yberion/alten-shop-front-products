6a5a3bb96d9736789f76219b1a9a7f60ecc22ae4
Yberion <Brandon.Largeau@gmail.com>
2024-07-17 13:02:57 +0200

    refactor: setup eventCoalescing

--- src/app/app-base.config.ts
+++ src/app/app-base.config.ts
@@ -1,5 +1,5 @@
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
-import { LOCALE_ID, type ApplicationConfig } from '@angular/core';
+import { LOCALE_ID, provideZoneChangeDetection, type ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { MessageService } from 'primeng/api';
 import { appRoutes } from './app.routes';
@@ -8,6 +8,7 @@ import { errorInterceptor } from './core/interceptors/error/error.interceptor';
 export const appBaseConfig: ApplicationConfig = {
   providers: [
     provideRouter(appRoutes),
+    provideZoneChangeDetection({ eventCoalescing: true }),
     provideHttpClient(withInterceptors([errorInterceptor])),
     { provide: LOCALE_ID, useValue: 'en' },
     MessageService,
